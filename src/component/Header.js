192e3b7b7067752a4011acdfdbe4e0e47959a906
jppatel1 <jaypatel10673@gmail.com>
2020-03-09 09:01:04 -0700

    added the logo to the nav bar

--- src/component/Header.js
+++ src/component/Header.js
@@ -8,7 +8,7 @@ function Header(){
 		<div>
 		    <nav>
                 <div className="nav-wrapper black lighten-2">
-                    <a href="index.html" class="brand-logo center">Logo</a>
+                    <a href="index.html" class="brand-logo center"><img src={require('./logo.png') }alt="My logo" height={"auto"} width={70} mode='fit'  /></a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                 </ul>
                </div>
