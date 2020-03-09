4ad4f837a722fa9f1659da4959e0a1fa6f6d2f3b
jppatel1 <jaypatel10673@gmail.com>
2020-03-09 08:46:02 -0700

    changed the first page to full size

--- src/component/Maincontent.js
+++ src/component/Maincontent.js
@@ -5,30 +5,30 @@ function MainContent(){
 	return(
 	<div className="container">
 		<form className="col s12">
-			<div className="row col s6">
-				<div className="input-field col s3">
+			<div className="row">
+				<div className="input-field col s6">
 					<h7>First Name</h7>
 					<input id="first_name" type="text" className="validate"></input>
 				</div>
-				<div className="input-field col s3">
+				<div className="input-field col s6">
 					<h7>Last Name</h7>
 					<input id="last_name" type="text" className="validate"></input>
 				</div>
 			</div>
 			<div className="row">
-				<div className="input-field col s6">
+				<div className="input-field col s12">
 					<h7>Email</h7>
 					<input id="email" type="email" className="validate"></input>
 				</div>
 			</div>
 			<div className="row">
-				<div className="input-field col s6">
+				<div className="input-field col s12">
 					<h7>confirm Email</h7>
 					<input id="Confirm-email" type="email" className="validate"></input>
 				</div>
 			</div>
 			<div className="row">
-				<div className="input-field col s6">
+				<div className="input-field col s12">
 					<h7>password</h7>
 					<input id="password" type="password" className="validate"></input>
 				</div>
